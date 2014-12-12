7e1c712352fa53b50afdfef6f75520092617f143
Alexander Jerneck <alexander.jerneck@gmail.com>
2014-12-12 18:01:47 -0500

    Exercise 1

--- exercises/HAD/Y2014/M02/D24/Exercise.hs
+++ exercises/HAD/Y2014/M02/D24/Exercise.hs
@@ -10,5 +10,6 @@ module HAD.Y2014.M02.D24.Exercise where
 -- >>> filterByPair [1, 2, 2, 2, 3, 3, 4]
 -- [2,2,3]
 
---filterByPair :: Find the most generic signature
-filterByPair = undefined
+filterByPair :: Ord a => [a] -> [a]
+filterByPair (x:y:ys) = if x==y then x: filterByPair (y:ys) else filterByPair (y:ys)
+filterByPair _ = []
